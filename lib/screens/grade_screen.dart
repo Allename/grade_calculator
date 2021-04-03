d43193c97f3eacbaed7ab065bcdf9e9080f56c9c
Allename <allenameanthony2000@gmail.com>
2021-04-03 21:48:09 +0100

    feat: update

--- lib/screens/grade_screen.dart
+++ lib/screens/grade_screen.dart
@@ -6,12 +6,77 @@ class GradeScreen extends StatefulWidget {
 }
 
 class _GradeScreenState extends State<GradeScreen> {
+  String valueChoose;
+  List listItem = [
+    'BLD232',
+    'BLD213',
+    'BLD214',
+    'BLD211',
+    'CEG211',
+    'SVY217',
+    'GEG217',
+    'ESM216',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Grade Calculator'),
       ),
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text('Course Codes'),
+                Text('Course Units'),
+                Text('Grade'),
+              ],
+            ),
+            SizedBox(height: 20.0),
+            Row(
+              children: <Widget>[
+                DropdownButton(
+                  hint: Text('Selecct course'),
+                  value: valueChoose,
+                  onChanged: (newValue) {
+                    setState(() {
+                      valueChoose = newValue;
+                    });
+                  },
+                  items: listItem.map((valueItem) {
+                    return DropdownButton<String>(
+                      value: valueItem,
+                      child: Text(valueItem),
+                    );
+                  }).toList(),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
+//child: Column(
+//children: <Widget>[
+//TextField(
+//keyboardType: TextInputType.text,
+//decoration: InputDecoration(
+//hintText: 'Enter course',
+//),
+//),
+//SizedBox(height: 20.0),
+//TextField(
+//keyboardType: TextInputType.number,
+//decoration: InputDecoration(
+//hintText: 'nter ',
+//),
+//),
+//],
+//),
